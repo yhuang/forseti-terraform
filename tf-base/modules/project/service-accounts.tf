7903544004d5b032df26d73c569be73db9b064a3
Jimmy Huang <Jimmy.Huang@creditkarma.com>
2018-04-24 03:50:08 -0700

    fixed logic bug

--- tf-base/modules/project/service-accounts.tf
+++ tf-base/modules/project/service-accounts.tf
@@ -5,8 +5,18 @@ resource "google_service_account" "auditor" {
   project      = "${google_project.project.project_id}"
 }
 
+resource "google_service_account" "enforcer" {
+  account_id   = "${var.roles["enforcer"]}"
+  project      = "${google_project.project.project_id}"
+}
 
-resource "google_organization_iam_binding" "auditor_bigquery_data_viewer" {
+resource "google_service_account" "explainer" {
+  account_id   = "${var.roles["explainer"]}"
+  project      = "${google_project.project.project_id}"
+}
+
+
+resource "google_organization_iam_binding" "bigquery_data_viewer" {
   org_id = "${var.org_id}"
   role   = "roles/bigquery.dataViewer"
 
@@ -15,7 +25,7 @@ resource "google_organization_iam_binding" "auditor_bigquery_data_viewer" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_browser" {
+resource "google_organization_iam_binding" "browser" {
   org_id = "${var.org_id}"
   role   = "roles/browser"
 
@@ -24,7 +34,7 @@ resource "google_organization_iam_binding" "auditor_browser" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_network_viewer" {
+resource "google_organization_iam_binding" "network_viewer" {
   org_id = "${var.org_id}"
   role   = "roles/compute.networkViewer"
 
@@ -33,7 +43,7 @@ resource "google_organization_iam_binding" "auditor_network_viewer" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_security_reviewer" {
+resource "google_organization_iam_binding" "security_reviewer" {
   org_id = "${var.org_id}"
   role   = "roles/iam.securityReviewer"
 
@@ -42,7 +52,7 @@ resource "google_organization_iam_binding" "auditor_security_reviewer" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_appengine_app_iewer" {
+resource "google_organization_iam_binding" "appengine_app_iewer" {
   org_id = "${var.org_id}"
   role   = "roles/appengine.appViewer"
 
@@ -51,7 +61,7 @@ resource "google_organization_iam_binding" "auditor_appengine_app_iewer" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_b_quota_viewer" {
+resource "google_organization_iam_binding" "b_quota_viewer" {
   org_id = "${var.org_id}"
   role   = "roles/servicemanagement.quotaViewer"
 
@@ -60,7 +70,7 @@ resource "google_organization_iam_binding" "auditor_b_quota_viewer" {
   ]
 }
 
-resource "google_organization_iam_binding" "auditor_cloudsql_viewer" {
+resource "google_organization_iam_binding" "cloudsql_viewer" {
   org_id = "${var.org_id}"
   role   = "roles/cloudsql.viewer"
 
@@ -70,38 +80,53 @@ resource "google_organization_iam_binding" "auditor_cloudsql_viewer" {
 }
 
 
-resource "google_project_iam_binding" "auditor_storage_object_viewer" {
+resource "google_organization_iam_binding" "security_admin" {
+  org_id = "${var.org_id}"
+  role   = "roles/compute.securityAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.enforcer.email}",
+  ]
+}
+
+
+resource "google_project_iam_binding" "storage_object_viewer" {
   project = "${google_project.project.project_id}"
   role    = "roles/storage.objectViewer"
 
   members = [
     "serviceAccount:${google_service_account.auditor.email}",
+    "serviceAccount:${google_service_account.enforcer.email}",
   ]
 }
 
-resource "google_project_iam_binding" "auditor_storage_object_creator" {
+resource "google_project_iam_binding" "storage_object_creator" {
   project = "${google_project.project.project_id}"
   role    = "roles/storage.objectCreator"
 
   members = [
     "serviceAccount:${google_service_account.auditor.email}",
+    "serviceAccount:${google_service_account.enforcer.email}",
   ]
 }
 
-resource "google_project_iam_binding" "auditor_log_writer" {
+resource "google_project_iam_binding" "log_writer" {
   project = "${google_project.project.project_id}"
   role    = "roles/logging.logWriter"
 
   members = [
     "serviceAccount:${google_service_account.auditor.email}",
+    "serviceAccount:${google_service_account.enforcer.email}",
   ]
 }
 
-resource "google_project_iam_binding" "auditor_cloudsql_client" {
+resource "google_project_iam_binding" "cloudsql_client" {
   project = "${google_project.project.project_id}"
   role    = "roles/cloudsql.client"
 
   members = [
     "serviceAccount:${google_service_account.auditor.email}",
+    "serviceAccount:${google_service_account.enforcer.email}",
+    "serviceAccount:${google_service_account.explainer.email}",
   ]
 }
