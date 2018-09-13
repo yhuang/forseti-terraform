a660f9004571a295aedb6357de8e124bafd86bc7
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-09-13 07:10:00 -0700

    broke out the image building portion into its own repo

--- packer/provisioning-scripts/install-cloud-sql-proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CLOUD_SQL_PROXY=/usr/local/bin/cloud_sql_proxy
-
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
-mv cloud_sql_proxy.linux.amd64 $CLOUD_SQL_PROXY
-chmod a+x $CLOUD_SQL_PROXY
