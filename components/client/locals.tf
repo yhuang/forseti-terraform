e4b4533d773fd5d66602fd265f8c6ec85035bb36
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-10-07 09:55:12 -0700

    DRY up the subnetwork self link references

--- components/client/locals.tf
+++ components/client/locals.tf
@@ -5,14 +5,8 @@ locals {
   network                = "${data.terraform_remote_state.networking.network_self_link}"
   project_id             = "${data.terraform_remote_state.project.project_id}"
   region                 = "${var.regions[var.region]}"
-  subnetwork             = "${local.subnetworks[local.region]}"
-  subnetworks            = {
-    "us-west1"    = "${data.terraform_remote_state.networking.subnetwork_us_west1_self_link}"
-    "us-central1" = "${data.terraform_remote_state.networking.subnetwork_us_central1_self_link}"
-    "us-east1"    = "${data.terraform_remote_state.networking.subnetwork_us_east1_self_link}"
-    "us-east4"    = "${data.terraform_remote_state.networking.subnetwork_us_east4_self_link}"
-  }
   startup_script         = "${data.template_file.configure_forseti_security_client.rendered}"
   states_bucket          = "${var.environment}-${var.project_name_base}-${var.states_bucket_base}"
+  subnetwork             = "${data.terraform_remote_state.networking.subnetwork_self_links[local.region]}"
   zone                   = "${element(var.availability_zones[local.region], 0)}"
 }
